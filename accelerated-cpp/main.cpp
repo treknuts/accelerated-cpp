4847f79466cfc4cf3dd91d0ceb6a78e8007e5ad9
treknuts <trevor.a.knutson@ndsu.edu>
2020-07-30 21:45:46 -0500

    Initial Commit

---
+++ accelerated-cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  accelerated-cpp
+//
+//  Created by Trevor Knutson on 7/30/20.
+//  Copyright © 2020 Trevor Knutson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
