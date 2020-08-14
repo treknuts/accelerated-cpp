9e0ad619c3ecd3374bd3db8acc07229a23bebd0c
treknuts <trevor.a.knutson@ndsu.edu>
2020-08-13 21:49:14 -0500

    Made functions for grade and median computations as well as a functions for reading homework grades.

--- accelerated-cpp/gradeCalculator.cpp
+++ accelerated-cpp/gradeCalculator.cpp
@@ -7,23 +7,62 @@
 //
 
 #include "gradeCalculator.hpp"
+#include "framedNameV2.hpp"
 #include <ios>
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <vector>
 #include <algorithm>
+#include <stdexcept>
 
 using std::cout;            using std::endl;
 using std::cin;             using std::sort;
 using std::setprecision;    using std::streamsize;
 using std::vector;          using std::string;
+using std::domain_error;    using std::istream;
+
+double grade(double midterm, double finalExam, double homework) {
+    return 0.2 * midterm + 0.4 * finalExam + 0.4 * homework;
+}
+
+double median(vector<double> vec) {
+    // vec_sz is type vector<double>::size_type
+    typedef vector<double>::size_type vec_sz;
+    vec_sz size = vec.size();
+    
+    sort(vec.begin(), vec.end());
+    
+    if (size == 0)
+        throw domain_error("Cannot find median of an empty vector!");
+    
+    vec_sz mid = size / 2;
+    
+    return size % 2 == 0 ? (vec[mid] + vec[mid + 1]) / 2 : vec[mid];
+    
+}
+
+double grade(double midterm, double finalExam, const vector<double>& hw) {
+    if (hw.size() == 0)
+        throw domain_error("The student has done no homework.");
+    return 0.2 * midterm + 0.4 * finalExam + 0.4 * median(hw);
+}
+
+istream& read_hw(istream& in, vector<double>& hw) {
+    if (in) {
+        hw.clear();
+        
+        double x;
+        while(in >> x)
+            hw.push_back(x);
+        
+        in.clear();
+    }
+    return in;
+}
 
 int calculateGrade() {
-    cout << "Please enter your first name: ";
-    string name;
-    cin >> name;
-    cout << "Hello, " + name + "!" << endl;
+    greetingV2();
     
     cout << "Please enter your midterm and final exam grades: ";
     double midterm, finalExam;
@@ -32,45 +71,13 @@ int calculateGrade() {
     cout << "Enter all your homework grades: "
             "followed by end-of-file";
     
-    /*
-     v1 of the program computed the average hw grade
-     Hence, why we needed 2 loop invariants
-     int count = 0;
-     double sum = 0;
-     */
-     double x;
     vector<double> homework;
     
-    while (cin >> x) {
-        /*
-         ++count;
-         sum += x;
-        */
-        homework.push_back(x);
-    }
-    
-    // A fancy way of aliasing vector<type>::size_type as vec_sz
-    typedef vector<double>::size_type vec_sz;
-    // vec_sz is type vector<double>::size_type
-    vec_sz size = homework.size();
-    
-    if (size == 0) {
-        cout << "You must enter your grades. "
-                "Please try again." << endl;;
-        return 1;
-    }
-    
-    sort(homework.begin(), homework.end());
-    
-    vec_sz mid = size / 2;
-    double median;
-    median = size % 2 == 0 ? (homework[mid] + homework[mid - 1]) / 2
-                           : homework[mid];
-    
+    read_hw(cin, homework);
     
     streamsize prec = cout.precision();
     cout << "Your final grade is " << setprecision(3)
-         << 0.2 * midterm + 0.4 * finalExam + 0.4 * median
+         << grade(midterm, finalExam, homework)
          << setprecision(prec) << endl;
     
     return 0;
